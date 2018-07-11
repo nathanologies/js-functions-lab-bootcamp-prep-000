8ef154992ee6eaff3ea6cf757b20fc9f466606b7
Nathan Lin <nlin@theticker.org>
2018-07-11 18:25:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function happyHolidays(string) {
 
 
 function happyHolidaysTo(name) {
-  return "Happy holidays, ${name}!"
+  return 'Happy holidays, ${name}!'
 }
 
 function happyHolidayTo(holiday, name) {
